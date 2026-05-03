d97019921777052c55e5c620037513e170fb593c
23wiro <william.1.roos@gmail.com>
2026-05-03 10:54:19 +0200

    bing

--- src/main/java/com/ssis/ssisauth/ExampleMod.java
+++ src/main/java/com/ssis/ssisauth/ExampleMod.java
@@ -92,6 +92,8 @@ public class ExampleMod {
         // Note that this is necessary if and only if we want *this* class (ExampleMod) to respond directly to events.
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         // In your constructor
+        // In your constructor, make sure this line exists:
+        NeoForge.EVENT_BUS.register(this);
         NeoForge.EVENT_BUS.addListener(this::onRegisterCommands);
 
         // Register the item to a creative tab
